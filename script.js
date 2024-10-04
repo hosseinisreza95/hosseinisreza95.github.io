a7575b8125fdc4cb62cff66121255d32306e36c7
Reza HOSSEINI <hosseinisreza95@gmail.com>
2024-10-04 23:33:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add any JavaScript interactivity here if needed
+document.querySelector('form').onsubmit = function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // Here you can add code to handle form submission
+};
