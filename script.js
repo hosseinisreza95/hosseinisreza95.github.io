df0c27a91be6fbc66af31228753411eb88601a46
Reza HOSSEINI <hosseinisreza95@gmail.com>
2024-10-08 13:07:29 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').onsubmit = function(event) {
-    event.preventDefault();
-    alert('Thank you for your message! I will get back to you soon.');
-    // Here you can add code to handle form submission or send data to your email.
-};
